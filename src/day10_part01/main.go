410f2c306315c00c16bd879391e316d0aec640a6
Nick Spoentgen <157921556+nspoentgen@users.noreply.github.com>
2024-12-20 18:03:47 -0800

    Day 10 part 1 solution

---
+++ src/day10_part01/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+type WorkItem struct {
+	visited map[[2]int]byte
+	position [2]int
+}
+
+func main() {
+	const FILEPATH string = `D:\Users\Nicolas\Documents\GoLandProjects\advent-of-code-2024\src\day10_part01\input.txt`
+	topoMap := parseData(FILEPATH)
+
+	sum := getTrailScoreSum(topoMap)
+	fmt.Printf("The sum is %d\n", sum)
+}
+
+func parseData(filepath string) [][]int {
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	topoMap := make([][]int, 0)
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lineVals := make([]int, 0)
+		line := scanner.Text()
+
+		for _, char := range line {
+			lineVals = append(lineVals, int(char - '0'))
+		}
+
+		topoMap = append(topoMap, lineVals)
+	}
+
+	return topoMap
+}
+
+func getTrailScoreSum(topoMap [][]int) int {
+	sum := 0
+
+	for row := range len(topoMap) {
+		for col := range len(topoMap[row]) {
+			if topoMap[row][col] == 0 {
+				sum += getTrailScore([2]int{row, col}, topoMap)
+			}
+		}
+	}
+
+	return sum
+}
+
+
+//Assumes starting at valid position
+func getTrailScore(position [2]int, topoMap [][]int) int {
+	score := 0
+	workStack := NewStack[*WorkItem]()
+	visited := make(map[[2]int]byte)
+	topPositions := make(map[[2]int]byte)
+	visited[position] = 0
+
+	workStack.Push(&WorkItem{visited: visited, position: position,})
+	workLeft := true
+
+	for workLeft {
+		val, err := workStack.Pop()
+		workLeft = err == nil
+
+		if workLeft{
+			for _, move := range getValidMoves(val.position, topoMap, val.visited) {
+				height := topoMap[move[0]][move[1]]
+
+				if height == 9 {
+					if _, contains := topPositions[move]; !contains {
+						topPositions[move] = 0
+						score++
+					}
+				} else {
+					visited = cloneMap(visited)
+					visited[move] = 0
+					workStack.Push(&WorkItem{visited: visited, position: move})
+				}
+			}
+		}
+	}
+
+	return score
+}
+
+func getValidMoves(position [2]int, topoMap [][]int, visited map[[2]int]byte) [][2]int {
+	var DELTAS = [4][2]int{
+		{-1, 0},
+		{1, 0},
+		{0, -1},
+		{0, 1}}
+
+	moves := make([][2]int, 0)
+	height := topoMap[position[0]][position[1]]
+
+	for _, delta := range DELTAS {
+		testPosition := [2]int{position[0] + delta[0], position[1] + delta[1]}
+		inBounds := testPosition[0] >= 0 && testPosition[0] < len(topoMap) &&
+			testPosition[1] >= 0 && testPosition[1] < len(topoMap[0])
+		_, haveVisited := visited[testPosition]
+
+		if inBounds && !haveVisited && topoMap[testPosition[0]][testPosition[1]] == height + 1 {
+			moves = append(moves, testPosition)
+		}
+	}
+
+	return moves
+}
+
+func cloneMap(input map[[2]int]byte) map[[2]int]byte {
+	clonedMap := make(map[[2]int]byte)
+
+	for k, v := range input {
+		clonedMap[k] = v
+	}
+
+	return clonedMap
+}
---
+++ src/day10_part01/util.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"sync"
+)
+
+type Stack[TData any] struct {
+	lock sync.Mutex // you don't have to do this if you don't want thread safety
+	s []TData
+}
+
+func NewStack[TData any]() *Stack[TData] {
+	return &Stack[TData]{sync.Mutex{}, make([]TData,0), }
+}
+
+func (s *Stack[TData]) Push(v TData) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	s.s = append(s.s, v)
+}
+
+func (s *Stack[TData]) Pop() (TData, error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+
+	l := len(s.s)
+	if l == 0 {
+		var dummy TData
+		return dummy, errors.New("Empty Stack")
+	}
+
+	res := s.s[l-1]
+	s.s = s.s[:l-1]
+	return res, nil
+}
